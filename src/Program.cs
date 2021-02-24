1915418283a0cac76dce7fcad86c101a553f49c4
Lars Bergström <lars.bergstrom@infovista.com>
2021-02-24 23:21:17 +0000

    Fixed the bug

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 using System;
-using System.Linq;
+using System.Data.SqlClient;
 using System.Reflection;
+using System.Threading;
 using CustomerContact.Utils;
 using DbUp;
 using Microsoft.AspNetCore.Hosting;
@@ -16,20 +17,33 @@ namespace CustomerContact
       // Create/migrate DB
       var connectionString = ConnectionString.Get();
 
-      EnsureDatabase.For.SqlDatabase(connectionString);
-
-      var upgrader =
-          DeployChanges.To
-              .SqlDatabase(connectionString)
-              .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-              .LogToConsole()
-              .Build();
-
-      var result = upgrader.PerformUpgrade();
-      if (!result.Successful)
+      while (true)
       {
-        Console.WriteLine(result.Error);
-        return;
+        try
+        {
+          EnsureDatabase.For.SqlDatabase(connectionString);
+
+          var upgrader =
+              DeployChanges.To
+                  .SqlDatabase(connectionString)
+                  .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                  .LogToConsole()
+                  .Build();
+
+          var result = upgrader.PerformUpgrade();
+          if (!result.Successful)
+          {
+            Console.WriteLine(result.Error);
+            return;
+          }
+
+          break;
+        }
+        catch (SqlException)
+        {
+          Console.WriteLine("DB not ready...");
+          Thread.Sleep(5000);
+        }
       }
 
       CreateHostBuilder(args).Build().Run();
